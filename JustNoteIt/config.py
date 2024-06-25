14216abb7a146e14e5da9165075809d5307c8a99
Jarae-MacyMan <sjaraebr@gmail.com>
2024-06-25 15:31:34 -0400

    api .env config

--- JustNoteIt/config.py
+++ JustNoteIt/config.py
@@ -1,20 +1,33 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import redis
+from dotenv import load_dotenv
 from os import environ
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__) #creates flask instance
 CORS(app, supports_credentials=True)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = r"sqlite:///mydatabase.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_ECHO"] = True
+# Database configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS", default=False)
+app.config["SQLALCHEMY_ECHO"] = os.getenv("SQLALCHEMY_ECHO", default=False)
 
-app.config["SESSION_TYPE"] = "redis"
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_USE_SIGNER"] = True
-app.config["SESSION_REDIS"] = redis.from_url("redis://127.0.0.1:6379")
+# Redis configuration
+redis_host = os.getenv("REDIS_HOST", "127.0.0.1")
+redis_port = os.getenv("REDIS_PORT", 6379)
+app.config["SESSION_REDIS"] = redis.from_url(f"redis://{redis_host}:{redis_port}")
+
+app.config["SESSION_TYPE"] = os.getenv("SESSION_TYPE", "redis")
+app.config["SESSION_PERMANENT"] = os.getenv("SESSION_PERMANENT", False)
+app.config["SESSION_USE_SIGNER"] = os.getenv("SESSION_USE_SIGNER", True)
+
+
+# app.config["SESSION_REDIS"] = redis.from_url("redis://127.0.0.1:6379")
 
 
 
