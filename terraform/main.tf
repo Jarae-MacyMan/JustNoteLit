0469c52b2a18557526fcc46f6982906103493b81
Jarae-MacyMan <sjaraebr@gmail.com>
2024-07-27 22:01:13 -0400

    terraform main file update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,91 @@
-#---root/main.tf---#
-
 provider "aws" {
-  region = local.location
+    region = "us-east-1"
 }
 
 
-locals {
-    cwd           = reverse(split("/", path.cwd))
-    instance_type = local.cwd[1]
-    location      = local.cwd[2]
-    environment   = local.cwd[3]
+resource "aws_instance" "app_server" {
+    ami = "ami-0427090fd1714168b"
+    instance_type = "t2.micro"
+    key_name = "notes-key-pair"
+
+    security_groups = [aws_security_group.instance_sg.name]
+
+    tags = {
+        Name = "notes-app-server"
+    }
+
+    user_data = <<-EOF
+        #!/bin/bash
+        sudo yum update -y
+        sudo yum install -y git docker
+        sudo service docker start
+        sudo usermod -a -G docker ec2-user
+        sudo curl -L https://github.com/docker/compose/releases/download/v2.29.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+        sudo systemctl enable docker
+        chmod 666 /var/run/docker.sock
+        cd /home/ec2-user
+        git clone https://github.com/Jarae-MacyMan/JustNoteLit
+        cd JustNoteLit
+        echo "Setup complete. Ready for manual docker-compose up."
+    EOF
+
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        private_key = file(var.private_key_path)
+        host = self.public_ip
+    }
 }
 
-module "instance" {
-    source = "../../../../../modules/dev/instance"
-    instance_type = local.instance_type
-    instance_count = 2
-    subnet_id = module.vpc.subnet_id
-}
+resource "aws_security_group" "instance_sg" {
+  name        = "allow_web_traffic"
+  description = "Allow HTTP and SSH traffic"
 
-module "vpc" {
-    source = "../../../../../modules/dev/vpc"
-    az = var.az
-    vpc_cidr_block = var.vpc_cidr_block
-    subnet_cidr_block = var.subnet_cidr_block
-}
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "React"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Flask API"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Redis"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}     
